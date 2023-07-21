25b8ed4ce750b23bcddc5ab9c35a0349f519d8e6
unknown <shanthala_ct@yahoo.co.in>
2023-07-21 16:01:54 +0100

    firstcommit

---
+++ shanthala/src/main/java/samudyata/shanthala/App.java
@@ -0,0 +1,13 @@
+package samudyata.shanthala;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
